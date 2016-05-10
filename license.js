74bf8df7061ccb443cafa14897c363533f6f7935
Alan Santos <asantos@assessor.lacounty.gov>
2016-05-10 13:48:04 -0700

    Bump version

--- license.js
+++ license.js
@@ -1,5 +1,5 @@
 /* 
 BSG utils
-LA County Assessor v1.1.0
+LA County Assessor v1.2.0
 Copyright (c) 2016 Los Angeles County Assessor, http://assessor.lacounty.gov/
 */
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Angular bsg utils
 
-**v.1.1.0**
+**v.1.2.0**
 
 Angular Assessor Utility service, directives and filters
 
